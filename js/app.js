82893a60536ad8ce287a76848cd3f397460ca4bc
Erin Lechler <erinlechler@gmail.com>
2020-09-17 17:57:21 -0500

    small css/js changes

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const alertBanner = document.getElementById("alert");
+alertBanner.innerHTML = 
+   `<div class="alert-banner">
+        <p><strong>Alert:</strong>You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">x</p>
+    </div>`
+
+    alertBanner.style.backgroundColor = '#7477BF';
+    
