b73f1d3f5749538de02deddefe7d1f66ff80c4fc
Erin Lechler <erinlechler@gmail.com>
2020-09-22 15:25:14 -0500

    js and extra credit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,53 @@
 //========================= ALERT BANNER =====================================================
 const alertBanner = document.getElementById("alert");
-alertBanner.innerHTML = "test";
-//    `<div class="alert">
-//         <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
-//         <p class="alert-banner-close"><strong>x</strong></p>
-//     </div>`;
+alertBanner.innerHTML = `<div class="alert"><p><strong>Alert</strong>: You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">X</p></div>`;
 
-    // alertBanner.style.backgroundColor = '#7477BF';
-    // alertBanner.style.color = "white";
-    // alertBanner.style.borderRadius = "4px";
-    ​
- alertBanner.addEventListener(​'click'​, e => {
-​   const​ element = e.target;
-​   if​ (element.classList.contains(​"alert-banner-close"​)) {
-    alert.style.display = ​"none" ;
-    }
+
+
+// alertBanner.style.display = "flex";
+// alertBanner.style.justifyContent = "space-between";
+alertBanner.style.width = "90%";
+
+alertBanner.style.backgroundColor = '#7477BF';
+alertBanner.style.color = "white";
+alertBanner.style.borderRadius = "4px";
+
+// alertBanner.style.margin = "10px";
+alertBanner.style.paddingRight = "10px";
+alertBanner.style.paddingLeft = "10px";
+alertBanner.style.padding = "5px";
+
+const closeBanner = document.getElementsByClassName("alert-banner-close");
+closeBanner[0].style.color = "red";
+
+closeBanner[0].addEventListener('click', () => {
+    alertBanner.style.display = 'none';
+    });
+//========================= NOTIFICATIONS -NEEDS TO BE AT THE SAME TIME ===========
+const notifications = document.getElementById('bell');
+
+function alertNotifications() {
+    alert('You have notifications.');
+    alert('Please complete your overdue tasks.')
+}
+        
+// notifications.addEventListener('click', () => {
+//         alert('You have notifications.');
+//         alert('Please complete your overdue tasks.')
+// })
+
+
+        notifications.addEventListener('click', (event) => {
+                    alertNotifications();
+                });
+
+
+
+
+
+
+
 
 //========================= LINE CHART =====================================================
 const lineData = {
@@ -47,6 +80,44 @@ const lineChart = new Chart(line, {
     data: lineData,
     options: lineOptions
 });
+
+//========================= 2 LINE CHART =====================================================
+const lineDataDaily = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-20', '11-17', '18-24', '25-31'],
+    datasets: [{
+        data: [550, 850, 1400, 400, 2500, 1350, 1150, 1650, 250, 500, 2000, 2100],
+        backgroundColor: 'red',
+        borderWidth: 1,
+    }]
+};
+// const lineOptions = {
+//     aspectRatio: 2.5,
+//     animation: {
+//         duration: 0
+//     },
+//     scales: {
+//         yAxes: [{
+//             ticks: {
+//                 beginAtZero: true
+//             }
+//         }]
+//     },
+//     legend: {
+//         display: false,
+//         }
+//     };
+const lineDaily = document.getElementById('trafficChartDaily').getContext('2d');
+const lineChartDaily = new Chart(lineDaily, {
+    type: 'line',
+    data: lineDataDaily,
+    options: lineOptions
+});
+
+// const dailyChart = document.getElementById('daily');
+// dailyChart.addEventListener('click', () => {
+//     lineChart.style.display = 'none';
+//     });
+
 //========================= BAR CHART =======================================================
 const barData = {
     labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
@@ -108,11 +179,11 @@ const donutChart = new Chart(donut, {
 const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
-//========================= not all if's work =====================================
+
 send.addEventListener('click', () => {
     if (user.value === '' && message.value === '') {
         alert('Please fill out user and message fields before sending')
-    } else if ( usser.value === '') {
+    } else if ( user.value === '') {
         alert('Please fill our user field before sending');
     } else if ( message.value === '') {
         alert('Please fill out message field before sending');
